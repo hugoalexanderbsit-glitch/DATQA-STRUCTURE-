3f10211055e00acab4c7178381cb2f7e66e0bd21
hugoalexanderbsit-glitch <hugoalexander_bsit@plmun.edu.ph>
2025-09-05 19:13:48 +0800

    Add files via upload

---
+++ DATASTR.py
@@ -0,0 +1,13 @@
+#start
+fn= input ("Input Full Name:")
+cs = input ("Input Course, Year and Section:")
+bday= input ("Input  Birthday:")
+motto=input ("Input Motto/Motivation in life:")
+
+
+print (f"\nHi! I'm {fn} of {cs}.")
+print ("Welcome to Data Structures and Algorithm")
+print (f"My Birthday is on {bday}.")
+print (f"And my motto/motivation in life is {motto}")
+#done
+
